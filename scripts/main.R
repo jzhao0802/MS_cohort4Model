cd019752cf5cf13d9ddeb748f83af667b2d99357
Lichao Wang kgxsapp100 <lichao.wang@uk.imshealth.com>
2016-07-07 15:15:28 +0100

    seems to be able to produce the result for the composite cohort.

---
+++ scripts/main.R
@@ -0,0 +1,292 @@
+library(dplyr)
+library(plyr)
+library(stringr)
+library(lazyeval)
+
+rm(list=ls())
+
+#
+# Steps to further clean the data for modelling:
+# 1. median impute the missing category of years_diag_idx and merge it with the others
+# 2. choose and then remove the reference categories
+# 3. Add 3 more variables about the sub-cohorts the records are from
+# 4. Save the data
+#
+
+
+
+FindBinVarReference_WithoutMissing <- function(
+  prefix, varNames, varValues
+  )
+{
+  # if the first character of all vars is a number, use the number
+  # else if the first two are c(ge, gt, le, lt), then use the le
+  # else return NA
+  
+  cateStart <- str_locate(varNames[1], prefix)[1, "end"] + 1
+  firstCharacters <- as.numeric(str_sub(varNames, start=cateStart, end=cateStart))
+  
+  if (!any(is.na(firstCharacters))) # if the first character of all vars is a number
+    refVarName <- varNames[which.min(firstCharacters)]
+  else
+  {
+    firstTwoCharacters <- str_sub(varNames, start=cateStart, end=cateStart+1)
+    if (all(firstTwoCharacters %in% c("ge", "le", "gt", "lt")))
+    {
+      IDs_le <- which(firstTwoCharacters == "le")
+      if (length(IDs_le) == 1)
+        refVarName <- varNames[IDs_le]
+      else
+        refVarName <- NA
+    }
+    else
+    {
+      refVarName <- NA
+    }
+      
+  }
+  
+  return (refVarName)
+    
+}
+
+FindBinVarReference <- function(prefix, allVarNames, dataset, minRefPct)
+{
+  categories <- allVarNames[grepl(prefix, allVarNames)]
+  #
+  # if there's a 'missing', use 'missing' unless it's less than minRefPct; 
+  # If the there's no missing, or missing is less than minRefPct, 
+  # Then look at first character after '__'. 
+  # If the first character of all other categories is a number, choose the smaller one;
+  # If not all first characters is a number, look at the first two characters, 
+  # If the first two are all %in% c(ge, gt, le, lt), then use the le;
+  # If none of the above applies, returns NA so that they can be manually addressed
+  # At the end before returning also check whether the reference is less than minRefPct, 
+  # if so then returns NA
+  #
+  
+  if (any(grepl(paste0(prefix, "missing"), allVarNames)))
+  {
+    missing_pct <- sum(dataset[, paste0(prefix, "missing")]) / nrow(dataset)
+    if (missing_pct < minRefPct) # check whether missing < 10%
+    {
+      categories <- categories[categories != "missing"]
+      refVarName <- FindBinVarReference_WithoutMissing(
+        prefix, categories, dataset[, categories]
+        )
+    } else
+    {
+      refVarName <- paste0(prefix, "missing")
+    }
+  } else
+  {
+    refVarName <- FindBinVarReference_WithoutMissing(
+      prefix, categories, dataset[, categories]
+      )
+  }
+  
+  if (!is.na(refVarName))
+  {
+    pctRef <- sum(dataset[, refVarName]) / nrow(dataset)
+    if (pctRef < minRefPct)
+      refVarName <- NA
+  }
+  
+  return (refVarName)
+}
+
+inputDir4DS <- "F:/Lichao/work/Projects/MultipleSclerosis/Results/2016-07-07/2016-07-07 12.24.06/"
+outcomes <- c("relapse_fu_any_01", "edssprog", "edssconf3",
+              "relapse_or_prog", "relapse_and_prog", "relapse_or_conf")
+
+orgCohort <- tbl_df(read.csv(paste0(inputDir4DS, "Cmp.csv"))) %>%
+  dplyr::rename(dayssup__gt360=dayssup_gt360) %>%
+  dplyr::rename(dayssup__le360=dayssup_le360)
+
+
+
+#
+## Impute the years_diag_idx
+
+
+
+missing_reps <- c('', 'NA', 'unknown', 'ambiguous')
+cohortDir_Jie <- "F:/Lichao/work/Projects/MultipleSclerosis/Results/2016-07-05/cohortDt_jie_July06/"
+medianYears <- median(
+  read.csv(
+    paste0(cohortDir_Jie, "dt_Cmp_withoutTransf.csv"), 
+    na.strings=missing_reps
+    )$years_diag_idx, na.rm=T
+  )
+if (medianYears <= 2)
+{
+  orgCohort[orgCohort$years_diag_idx__missing==1, "years_diag_idx__le2"] <- 1
+} else if (medianYears <= 5)
+{
+  orgCohort[orgCohort$years_diag_idx__missing==1, "years_diag_idx__gt2_le5"] <- 1
+} else
+  orgCohort[orgCohort$years_diag_idx__missing==1, "years_diag_idx__gt5"] <- 1
+
+orgCohort <- select(orgCohort, -years_diag_idx__missing)
+
+
+
+#
+## find and remove reference categories
+
+
+
+pct <- 0.15
+
+referencesForBinVars <- 
+  colnames(orgCohort) %>%
+  str_extract(".*__") %>% # extract prefix
+  .[!(is.na(.))] %>% # remove output vars
+  unique %>%
+  {
+    uniquePrefixes <<- .
+    .
+  } %>%
+  lapply(FindBinVarReference, allVarNames=colnames(orgCohort), dataset=orgCohort, 
+         minRefPct=pct) %>%
+  ldply(c)
+
+
+naIndices <- which(is.na(referencesForBinVars))
+remainPrefixes <- uniquePrefixes[naIndices]
+refs4RemainPrefixes <- c(
+  "age__le30",
+  "baseline_edss_score__0_1",
+  "birth_region__Central_Europe",
+  "gender__F"
+)
+
+referencesForBinVars[is.na(referencesForBinVars)] <- refs4RemainPrefixes
+
+# remove ref categories
+
+remainVarNames <- colnames(orgCohort)[!(colnames(orgCohort) %in% referencesForBinVars$V1)]
+
+recordIDColName <- "record_num"
+refRemovedCohort <- select_(orgCohort, .dots=c(remainVarNames, recordIDColName))
+
+
+
+#
+## add cohort flag
+
+
+# be careful when changing the order of the names in subCohortNames
+# it could affect functions below using them!
+subCohortNames <- c("BConti", "B2B", "B2Fir", "B2Sec") 
+rawDataFile <- "F:/Lichao/work/Projects/MultipleSclerosis/data/2016-07-01/MS_decsupp_analset_20160701.csv"
+rawData <- tbl_df(read.csv(rawDataFile, na.strings=missing_reps))
+# recordIDs_AllSubCohorts <- list()
+# for (subCohortName in subCohortNames)
+# {
+#   recordIDs_AllSubCohorts[[subCohortName]] <-
+#     read.csv(paste0(inputDir4DS, subCohortName, ".csv"))[, recordIDColName]
+# }
+# # check: is there any overlap between IDs from different cohort?
+# for (name1 in subCohortNames[1:3])
+# {
+#   for (name2 in subCohortNames[2:4])
+#   {
+#     if (name1 != name2)
+#     {
+#       if (length(intersect(recordIDs_AllSubCohorts[[name1]], recordIDs_AllSubCohorts[[name2]])) != 0)
+#         stop(paste0("Error! There's overlap in record IDs between ", name1, " and ", name2, "."))
+#     }
+#   }
+# }
+
+FindSubCohortOneRecord <- function(tblcoh)
+{
+  return (subCohortNames[tblcoh])
+}
+
+cohortFlagAppendedData <- 
+  refRemovedCohort %>%
+  {
+    .[, subCohortNames] <- 0
+    
+    for (i in 1:nrow(.))
+    {
+      record_id <- as.data.frame(.)[i, recordIDColName]
+      subCohortThisRecord <- FindSubCohortOneRecord(rawData$tblcoh[record_id])
+      
+      .[i, subCohortThisRecord] <- 1
+    }
+    .
+  }
+#   {
+#     varval <- interp(quote(ifelse(recordIDColName %in% recordIDs_AllSubCohorts[[subCohortNames[1]]], 1, 0)), 
+#                      recordIDColName=recordIDColName, recordIDs_AllSubCohorts=recordIDs_AllSubCohorts, 
+#                      subCohortNames=subCohortNames)
+#     aa<<-mutate_(., .dots= setNames(varval, subCohortNames[1]))
+#   } %>%
+#   {
+#     varval <- interp(quote(ifelse(recordIDColName %in% recordIDs_AllSubCohorts[[subCohortNames[2]]], 1, 0)), 
+#                      recordIDColName=recordIDColName, recordIDs_AllSubCohorts=recordIDs_AllSubCohorts, 
+#                      subCohortNames=subCohortNames)
+#     mutate_(., .dots= setNames(list(varval), subCohortNames[2]))
+#   } %>%
+#   {
+#     varval <- interp(quote(ifelse(recordIDColName %in% recordIDs_AllSubCohorts[[subCohortNames[3]]], 1, 0)), 
+#                      recordIDColName=recordIDColName, recordIDs_AllSubCohorts=recordIDs_AllSubCohorts, 
+#                      subCohortNames=subCohortNames)
+#     mutate_(., .dots= setNames(list(varval), subCohortNames[3]))
+#   } %>%
+#   {
+#     varval <- interp(quote(ifelse(recordIDColName %in% recordIDs_AllSubCohorts[[subCohortNames[4]]], 1, 0)), 
+#                      recordIDColName=recordIDColName, recordIDs_AllSubCohorts=recordIDs_AllSubCohorts, 
+#                      subCohortNames=subCohortNames)
+#     mutate_(., .dots= setNames(list(varval), subCohortNames[4]))
+#   }
+# 
+#   
+#   mutate_(B2B = ifelse(recordIDColName %in% recordIDs_AllSubCohorts[["B2B"]], 1, 0)) %>%
+#   mutate_(B2Fir = ifelse(recordIDColName %in% recordIDs_AllSubCohorts[["B2Fir"]], 1, 0)) %>%
+#   mutate_(B2Sec = ifelse(recordIDColName %in% recordIDs_AllSubCohorts[["B2Sec"]], 1, 0)) %>%
+#   mutate_(BConti = ifelse(recordIDColName %in% recordIDs_AllSubCohorts[["BConti"]], 1, 0))
+  
+# CheckOneFlag <- function(subCohortName, allSubCohortNames, rowIdx, recordIDs, data)
+# {
+#   # if this record belongs to a subcohort, the flag of that subcohort has to be 1
+#   # while others must be 0.
+#   otherCohortNames <- allSubCohortNames[allSubCohortNames != subCohortName]
+#   if (data[rowIdx, recordIDColName] %in% recordIDs[[subCohortName]])
+#   {
+#     if ((data[rowIdx, subCohortName] != 1) | any(data[rowIdx, otherCohortNames]))
+#       stop(paste0("Error! Row ", rowIdx, " in the data has incorrect sub-cohort flags."))
+#   }
+#   # in this row there must be one and at least one value 1
+#   if (!all(data[rowIdx, allSubCohortNames] %in% c(0,1)))
+#     stop(paste0("Error! Row ", rowIdx, " has values other 0 and 1."))
+#   if (sum(data[rowIdx, allSubCohortNames]) != 1)
+#     stop(paste0("Error! Row ", rowIdx, " doesn't sum up to 1."))
+# }
+
+CheckCohortFlags <- function(rowIdxInProcessedData, vec_tblcoh, processedData)
+{
+  record_idx <- as.data.frame(processedData)[rowIdxInProcessedData, recordIDColName]
+  subCohort <- subCohortNames[vec_tblcoh[record_idx]]
+  otherCohorts <- subCohortNames[subCohortNames != subCohort]
+  
+  if ((processedData[rowIdxInProcessedData, subCohort] != 1) | 
+    (any(processedData[rowIdxInProcessedData, otherCohorts])))
+    stop(paste0("Error! row ", rowIdxInProcessedData, " has incorrect sub-cohort flags."))
+}
+
+# check whether each record is only in one cohort
+lapply(
+  (1:nrow(cohortFlagAppendedData)), 
+  CheckCohortFlags, 
+  vec_tblcoh=rawData$tblcoh, 
+  processedData=cohortFlagAppendedData[, c(recordIDColName, subCohortNames)]
+  )
+
+
+
+
+# check whether the cohorts are the same as what Jie sent me
